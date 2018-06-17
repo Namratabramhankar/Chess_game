4312adc29d591634d2ca17a0433e6863d31325d9
Namrata <namratabramhankar@gmail.com>
2018-06-17 11:55:30 -0700

    commiting

--- app/src/main/java/com/coc/chessgame/MainActivity.java
+++ app/src/main/java/com/coc/chessgame/MainActivity.java
@@ -2,12 +2,50 @@ package com.coc.chessgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button[][] buttons = new Button[8][8];
+
+    private TextView textViewPlayer1;
+    private TextView textViewPlayer2;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewPlayer1 = findViewById(R.id.text_view_p1);
+        textViewPlayer2 = findViewById(R.id.text_view_p2);
+
+        for(int i=0; i<8; ++i){
+            for (int j=0; j<8; ++j){
+                String buttonId = "button_" + i + j;
+                int resId = getResources().getIdentifier(buttonId,"id", getPackageName());
+                buttons[i][j] = findViewById(resId);
+                buttons[i][j].setOnClickListener(this);
+
+            }
+        }
+
+        Button buttonReset = findViewById(R.id.button_reset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
