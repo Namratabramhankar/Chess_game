495be1089dcfbb32e75a6f671a781d0605a7b988
Namrata <namratabramhankar@gmail.com>
2018-06-29 20:07:07 -0700

    commiting

--- app/src/main/java/com/coc/chessgame/MainActivity.java
+++ app/src/main/java/com/coc/chessgame/MainActivity.java
@@ -4,14 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private Button[][] buttons = new Button[8][8];
+
 
     private TextView textViewPlayer1;
     private TextView textViewPlayer2;
+
+    private ImageButton[][] buttons = new ImageButton[8][8];
+    boolean ispressed = false;
+
+
 
 
 
@@ -24,15 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         textViewPlayer1 = findViewById(R.id.text_view_p1);
         textViewPlayer2 = findViewById(R.id.text_view_p2);
 
-        for(int i=0; i<8; ++i){
-            for (int j=0; j<8; ++j){
-                String buttonId = "button_" + i + j;
-                int resId = getResources().getIdentifier(buttonId,"id", getPackageName());
+        for (int i = 0; i < 8; ++i){
+            for (int j = 0; j < 8; ++j ){
+                String buttonId = "Button_" + i + j;
+                int resId = getResources().getIdentifier(buttonId,"id",getPackageName());
                 buttons[i][j] = findViewById(resId);
                 buttons[i][j].setOnClickListener(this);
-
             }
         }
+
+
 
         Button buttonReset = findViewById(R.id.button_reset);
         buttonReset.setOnClickListener(new View.OnClickListener() {
@@ -41,11 +48,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             }
         });
+        }
 
-    }
 
     @Override
     public void onClick(View v) {
+        for (int i = 0; i < 8; ++i){
+            for (int j = 0; j < 8; ++j){
+                buttons[i][j].setTag(i+j);
+                }
+        }
+        if (!ispressed){
+            for (int i = 0; i < 8; ++i){
+                for (int j = 0; j < 8; ++j){
+                    if ((i + j)%2 == 0){
+                        buttons[i][j].setBackgroundResource(R.drawable.black);
+                    }else {
+                        buttons[i][j].setBackgroundResource(R.drawable.white);
+                    }
+                    buttons[i][j].setImageDrawable(null);
+                }
+            }
+        }
+
 
     }
 }
+
