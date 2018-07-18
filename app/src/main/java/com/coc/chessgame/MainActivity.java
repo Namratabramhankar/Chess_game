20fc1723d31c9b3d32bc7017af23855c89f07c56
Namrata <namratabramhankar@gmail.com>
2018-07-17 22:58:14 -0700

    commiting

--- app/src/main/java/com/coc/chessgame/MainActivity.java
+++ app/src/main/java/com/coc/chessgame/MainActivity.java
@@ -17,8 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ImageButton[][] buttons = new ImageButton[8][8];
     private ImageButton selected = null;
-    Location selectedLocation;
-
+    private final static String brown = "#a17948";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,59 +39,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < 8; ++j) {
                 if (((i + j) % 2) == 0) {
-                    buttons[i][j].setBackgroundResource(R.drawable.black);
+                    buttons[i][j].setBackgroundColor(Color.GRAY);
                 } else {
-                    buttons[i][j].setBackgroundResource(R.drawable.white);
+                    buttons[i][j].setBackgroundColor(Color.WHITE);
                 }
                 //selected = null;
             }
         }
 
 
-        buttons[0][0].setImageResource(R.drawable.rook3);
-        buttons[0][0].setTag(Integer.valueOf(R.drawable.rook3));
-        buttons[0][7].setImageResource(R.drawable.rook3);
-        buttons[0][7].setTag(Integer.valueOf(R.drawable.rook3));
-        buttons[0][1].setImageResource(R.drawable.night2);
-        buttons[0][1].setTag(Integer.valueOf(R.drawable.night2));
-        buttons[0][6].setImageResource(R.drawable.night2);
-        buttons[0][6].setTag(Integer.valueOf(R.drawable.night2));
-
-        buttons[0][2].setImageResource(R.drawable.bishop2);
-        buttons[0][2].setTag(Integer.valueOf(R.drawable.bishop2));
-        buttons[0][5].setImageResource(R.drawable.bishop2);
-        buttons[0][5].setTag(Integer.valueOf(R.drawable.bishop2));
-
-        buttons[0][3].setImageResource(R.drawable.queen2);
-        buttons[0][3].setTag(Integer.valueOf(R.drawable.queen2));
-        buttons[0][4].setImageResource(R.drawable.king2);
-        buttons[0][4].setTag(Integer.valueOf(R.drawable.king2));
-
-        buttons[7][0].setImageResource(R.drawable.rook1);
-        buttons[7][0].setTag(Integer.valueOf(R.drawable.rook1));
-        buttons[7][7].setImageResource(R.drawable.rook1);
-        buttons[7][7].setTag(Integer.valueOf(R.drawable.rook1));
-        buttons[7][1].setImageResource(R.drawable.night1);
-        buttons[7][1].setTag(Integer.valueOf(R.drawable.night1));
-        buttons[7][6].setImageResource(R.drawable.night1);
-        buttons[7][6].setTag(Integer.valueOf(R.drawable.night1));
-
-        buttons[7][2].setImageResource(R.drawable.bishop1);
-        buttons[7][2].setTag(Integer.valueOf(R.drawable.bishop1));
-        buttons[7][5].setImageResource(R.drawable.bishop1);
-        buttons[7][5].setTag(Integer.valueOf(R.drawable.bishop1));
-
-        buttons[7][3].setImageResource(R.drawable.queen1);
-        buttons[7][3].setTag(Integer.valueOf(R.drawable.queen1));
-
-        buttons[7][4].setImageResource(R.drawable.king1);
-        buttons[7][4].setTag(Integer.valueOf(R.drawable.king1));
+        buttons[0][0].setImageResource(R.drawable.rookw);
+        buttons[0][0].setTag(Integer.valueOf(R.drawable.rookw));
+        buttons[0][7].setImageResource(R.drawable.rookw);
+        buttons[0][7].setTag(Integer.valueOf(R.drawable.rookw));
+        buttons[0][1].setImageResource(R.drawable.nightw);
+        buttons[0][1].setTag(Integer.valueOf(R.drawable.nightw));
+        buttons[0][6].setImageResource(R.drawable.nightw);
+        buttons[0][6].setTag(Integer.valueOf(R.drawable.nightw));
+        buttons[0][2].setImageResource(R.drawable.bishopw);
+        buttons[0][2].setTag(Integer.valueOf(R.drawable.bishopw));
+        buttons[0][5].setImageResource(R.drawable.bishopw);
+        buttons[0][5].setTag(Integer.valueOf(R.drawable.bishopw));
+        buttons[0][3].setImageResource(R.drawable.queenw);
+        buttons[0][3].setTag(Integer.valueOf(R.drawable.queenw));
+        buttons[0][4].setImageResource(R.drawable.kingw);
+        buttons[0][4].setTag(Integer.valueOf(R.drawable.kingw));
+        buttons[7][0].setImageResource(R.drawable.rookb);
+        buttons[7][0].setTag(Integer.valueOf(R.drawable.rookb));
+        buttons[7][7].setImageResource(R.drawable.rookb);
+        buttons[7][7].setTag(Integer.valueOf(R.drawable.rookb));
+        buttons[7][1].setImageResource(R.drawable.nightb);
+        buttons[7][1].setTag(Integer.valueOf(R.drawable.nightb));
+        buttons[7][6].setImageResource(R.drawable.nightb);
+        buttons[7][6].setTag(Integer.valueOf(R.drawable.nightb));
+        buttons[7][2].setImageResource(R.drawable.bishopb);
+        buttons[7][2].setTag(Integer.valueOf(R.drawable.bishopb));
+        buttons[7][5].setImageResource(R.drawable.bishopb);
+        buttons[7][5].setTag(Integer.valueOf(R.drawable.bishopb));
+        buttons[7][3].setImageResource(R.drawable.queenb);
+        buttons[7][3].setTag(Integer.valueOf(R.drawable.queenb));
+        buttons[7][4].setImageResource(R.drawable.kingb);
+        buttons[7][4].setTag(Integer.valueOf(R.drawable.kingb));
 
         for (int i = 0; i < 8; ++i) {
-            buttons[1][i].setImageResource(R.drawable.pawn2);
-            buttons[1][i].setTag(Integer.valueOf(R.drawable.pawn2));
-            buttons[6][i].setImageResource(R.drawable.pawn1);
-            buttons[6][i].setTag(Integer.valueOf(R.drawable.pawn1));
+            buttons[1][i].setImageResource(R.drawable.pawnw);
+            buttons[1][i].setTag(Integer.valueOf(R.drawable.pawnw));
+            buttons[6][i].setImageResource(R.drawable.pawnb);
+            buttons[6][i].setTag(Integer.valueOf(R.drawable.pawnb));
         }
     }
 
@@ -134,56 +127,56 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             int tag = (Integer) selected.getTag();
             Log.v("tag", Integer.valueOf(tag).toString());
             switch (tag) {
-                case (R.drawable.rook1):
-                    next.setImageResource(R.drawable.rook1);
-                    next.setTag(Integer.valueOf(R.drawable.rook1));
+                case (R.drawable.rookw):
+                    next.setImageResource(R.drawable.rookw);
+                    next.setTag(Integer.valueOf(R.drawable.rookw));
                     break;
 
-                case (R.drawable.rook3):
-                    next.setImageResource(R.drawable.rook3);
-                    next.setTag(Integer.valueOf(R.drawable.rook3));
+                case (R.drawable.rookb):
+                    next.setImageResource(R.drawable.rookb);
+                    next.setTag(Integer.valueOf(R.drawable.rookb));
                     break;
 
-                case (R.drawable.bishop1):
-                    next.setImageResource(R.drawable.bishop1);
-                    next.setTag(Integer.valueOf(R.drawable.bishop1));
+                case (R.drawable.bishopw):
+                    next.setImageResource(R.drawable.bishopw);
+                    next.setTag(Integer.valueOf(R.drawable.bishopw));
                     break;
 
-                case (R.drawable.bishop2):
-                    next.setImageResource(R.drawable.bishop2);
-                    next.setTag(Integer.valueOf(R.drawable.bishop2));
+                case (R.drawable.bishopb):
+                    next.setImageResource(R.drawable.bishopb);
+                    next.setTag(Integer.valueOf(R.drawable.bishopb));
                     break;
-                case (R.drawable.king1):
-                    next.setImageResource(R.drawable.king1);
-                    next.setTag(Integer.valueOf(R.drawable.king1));
+                case (R.drawable.kingw):
+                    next.setImageResource(R.drawable.kingw);
+                    next.setTag(Integer.valueOf(R.drawable.kingw));
                     break;
-                case (R.drawable.king2):
-                    next.setImageResource(R.drawable.king2);
-                    next.setTag(Integer.valueOf(R.drawable.king2));
+                case (R.drawable.kingb):
+                    next.setImageResource(R.drawable.kingb);
+                    next.setTag(Integer.valueOf(R.drawable.kingb));
                     break;
-                case (R.drawable.queen1):
-                    next.setImageResource(R.drawable.queen1);
-                    next.setTag(Integer.valueOf(R.drawable.queen1));
+                case (R.drawable.queenw):
+                    next.setImageResource(R.drawable.queenw);
+                    next.setTag(Integer.valueOf(R.drawable.queenw));
                     break;
-                case (R.drawable.queen2):
-                    next.setImageResource(R.drawable.queen2);
-                    next.setTag(Integer.valueOf(R.drawable.queen2));
+                case (R.drawable.queenb):
+                    next.setImageResource(R.drawable.queenb);
+                    next.setTag(Integer.valueOf(R.drawable.queenb));
                     break;
-                case (R.drawable.night1):
-                    next.setImageResource(R.drawable.night1);
-                    next.setTag(Integer.valueOf(R.drawable.night1));
+                case (R.drawable.nightw):
+                    next.setImageResource(R.drawable.nightw);
+                    next.setTag(Integer.valueOf(R.drawable.nightw));
                     break;
-                case (R.drawable.night2):
-                    next.setImageResource(R.drawable.night2);
-                    next.setTag(Integer.valueOf(R.drawable.night2));
+                case (R.drawable.nightb):
+                    next.setImageResource(R.drawable.nightb);
+                    next.setTag(Integer.valueOf(R.drawable.nightb));
                     break;
-                case (R.drawable.pawn1):
-                    next.setImageResource(R.drawable.pawn1);
-                    next.setTag(Integer.valueOf(R.drawable.pawn1));
+                case (R.drawable.pawnw):
+                    next.setImageResource(R.drawable.pawnw);
+                    next.setTag(Integer.valueOf(R.drawable.pawnw));
                     break;
-                case (R.drawable.pawn2):
-                    next.setImageResource(R.drawable.pawn2);
-                    next.setTag(Integer.valueOf(R.drawable.pawn2));
+                case (R.drawable.pawnb):
+                    next.setImageResource(R.drawable.pawnb);
+                    next.setTag(Integer.valueOf(R.drawable.pawnb));
                     break;
             }
             selected.setImageDrawable(null);
@@ -193,6 +186,4 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         }
     }
-
-
 }
--- app/src/main/java/com/coc/chessgame/Piece.java
+++
@@ -1,4 +0,0 @@
-package com.coc.chessgame;
-
-class Piece {
-}
